f9788e983425d00dd6616cf56af7e383b67ea7b4
Phillips Oluwatomisin <Phillipspokuaa@gmail.com>
2023-06-30 18:01:16 +0100

    Function that returns the number of elements in a linked list_t list.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 /**
  * print_list - prints all the elements of a linked list
@@ -10,13 +10,9 @@ size_t print_list(const list_t *h)
 	size_t n = 0;
 
 	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
+        {
 		n++;
+		h = h->next;
 	}
 	return (n);
 }
